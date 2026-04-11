223fb250feff6b2d94fad9f04e7c493da9fabee4
RamCharan107h34 <24eg107h34@anurag.edu.in>
2026-04-12 00:21:40 +0530

    Added week-6

---
+++ week-6/Emp-project/emp-backend/server.js
@@ -0,0 +1,41 @@
+import exp from "express";
+import { connect } from "mongoose";
+import { empRoute } from "./API/empApp.js";
+import  {config} from 'dotenv'
+import cors from "cors";
+config()
+
+const app = exp();
+//add cors middleware
+app.use(
+  cors({
+    origin: [process.env.FRONT_END_URL],
+  }),
+);
+//body parser middleware
+app.use(exp.json());
+//emp api middleware
+app.use("/emp-api", empRoute);
+
+//DB connection
+const connectDB = async () => {
+  try {
+    await connect(process.env.DB_URL);
+    console.log("DB connected");
+    app.listen(process.env.PORT, () => console.log("server listening on port 4000.."));
+  } catch (err) {
+    console.log("err in DB connection", err.message);
+  }
+};
+
+connectDB();
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.log("err in middleware:", err.message);
+
+  res.status(err.status || 500).json({
+    message: "error",
+    reason: err.message,
+  });
+});
---
+++ week-6/counter-context-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+import { counterContextObj } from './components/CounterContextProvider'
+import RouterLayout from './components/RouterLayout'
+
+
+
+function App() {
+  return (
+    < RouterLayout/>
+  )
+}
+
+export default App
