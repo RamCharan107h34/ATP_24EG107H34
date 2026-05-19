a66a319e0ac2ef34d1b81944d301dd52afbe857e
RamCharan107h34 <24eg107h34@anurag.edu.in>
2026-05-19 08:58:33 +0530

    latest commit

--- week-6/Emp-project/emp-frontend/src/components/Home.jsx
+++ week-6/Emp-project/emp-frontend/src/components/Home.jsx
@@ -10,25 +10,21 @@ function Home () {
   const newCounter = useCounterStore((state)=>state.newCounter);
   const incrementCounter = useCounterStore((state)=>state.incrementCounter);
   const {counter,counter1,changeCounter,changeCounter1} = useContext(counterContextObj);
-
+  const decrementCounter = useCounterStore((state)=>state.decrementCounter); 
 
   console.log("Home")
 
   return (
     <div className="text-2xl space-y-4">
       <div>
-        <h1>Count : {newCounter}</h1>
-        <button onClick={incrementCounter} className="bg-blue-300 px-5 py-2 mt-2">incrementCounter</button>
-      </div>
-      <div>
-        <p>Context counter: {counter}</p>
+        <p>counter: {counter}</p>
         <button onClick={changeCounter} className="bg-green-300 px-5 py-2 mt-2">changeCounter</button>
       </div>
       <div>
-        <p>Context counter1: {counter1}</p>
-        <button onClick={changeCounter1} className="bg-yellow-300 px-5 py-2 mt-2">changeCounter1</button>
+        <h1>New Counter : {newCounter}</h1>
+        <button onClick={incrementCounter} className="bg-blue-300 px-5 py-2 mt-2">incrementCounter</button>
+        <button onClick={decrementCounter} className="bg-red-300 px-5 py-2 mt-2">decrementCounter</button>
       </div>
-      <Test />
     </div>
   )
 }
